994fae2ddce985cfcf820ecb9b885bbd3c2e0fdb
slekram <maxi.markels@gmail.com>
2022-06-12 21:47:39 -0300

    cree el repositorio y desafio 1

---
+++ src/Caja.js
@@ -0,0 +1,11 @@
+function Caja(props) {
+    return (
+        <div className="Caja">
+            <p>
+                Bienvenido {props.nombre} !
+            </p>
+        </div>
+    );
+}
+
+export default Caja;
